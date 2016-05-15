add3530910d8446d264634c29fc963e9908e2d30
Saleh Ahmad <salehoyon@hotmail.com>
2016-05-16 00:37:44 +0600

    * PSR Update

--- controller/addCourseTeacher.php
+++ controller/addCourseTeacher.php
@@ -5,7 +5,7 @@ require '../model/db.php';
 require 'define.php';
 
 /**
- * Assign Course to the Teacher
+ * Authority Assigns Course to the Teacher
  */
 if (isset($_POST['addCot'])) {
     $id          = $_POST['id2'];
@@ -19,29 +19,29 @@ if (isset($_POST['addCot'])) {
     $course = $_POST['subject'];
 
     if ($_POST['teacher'] != '' && $_POST['subject'] != '') {
-        if (!checkDuplicateCourse($tid, $course)) {
-            if (!checkCourse($course)) {
-                insertCourseTeacher($course, $tid);
+        if (!checkDuplicateCourse($tid, $course)) {  //===== Checking if the Course is Assigned before to this Teacher =====
+            if (!checkCourse($course)) {  //===== Checking if the Course is assigned before =====
+                insertCourseTeacher($course, $tid);  //===== Assigning Course to the Teacher =====
                 echo '<script language="javascript">
-                        alert("Successfully Assigned !!");
-                        window.location="'.SERVER.'/addSubject?id='.$id.'";
+                          alert("Successfully Assigned !!");
+                          window.location="'.SERVER.'/addSubject?id='.$id.'";
                       </script>';
             } else {
                 echo '<script language="javascript">
-                        alert("A teacher already assigned in this course !!");
-                        window.location="'.SERVER.'/addSubject?id='.$id.'";
+                          alert("A teacher already assigned in this course !!");
+                          window.location="'.SERVER.'/addSubject?id='.$id.'";
                       </script>';
             }
         } else {
             echo '<script language="javascript">
-                        alert("Duplicate Data !! Can\'t insert !!");
-                        window.location="'.SERVER.'/addSubject?id='.$id.'";
-                      </script>';
+                      alert("Duplicate Data !! Can\'t insert !!");
+                      window.location="'.SERVER.'/addSubject?id='.$id.'";
+                  </script>';
         }
     } else {
         echo '<script language="javascript">
-                        alert("You\'ve to fill both teacher and course");
-                        window.location="'.SERVER.'/addSubject?id='.$id.'";
-                      </script>';
+                  alert("You\'ve to fill both teacher and course");
+                  window.location="'.SERVER.'/addSubject?id='.$id.'";
+              </script>';
     }
 }
--- controller/addStudentSuccess.php
+++ controller/addStudentSuccess.php
@@ -1,47 +1,50 @@
 <?php
-	session_start();
-	require '../model/db.php';
-	require 'define.php';
+
+/*===== Session Starts =====*/
+session_start();
+
+/*===== Required Files =====*/
+require '../model/db.php';
+require 'define.php';
+
+if (isset($_POST['addStud'])) {
+	$stuId = $_POST['studId'];
+	$cid   = $_POST['courseId'];
 	
-	if(isset($_POST['addStud'])){
-		
-		$stuId = $_POST['studId'];
-		$cid = $_POST['courseId'];
-		if(checkStudentId($stuId)){
-			$tid = $_SESSION['tid'];
-			if(checkUniqueId($tid, $stuId, $cid)){
-				if(checkFourtyStud($cid)){
-					addStudent($tid, $stuId, $cid);
-					attendence($stuId, $cid);
-					addMarks($stuId, $cid);
-					
-					echo '<script language="javascript">
-							alert("Successfully Added");
-							window.location="'.SERVER.'/teacherCourse?id='.$cid.'";
-						  </script>';
-				}
-				else{			
-					echo '<script language="javascript">
-							alert("You cannot enter more than 40 students !!");
-							window.location="'.SERVER.'/teacherCourse?id='.$cid.'";
-						  </script>';
-				}
-			}
-			else{
+	if (checkStudentId($stuId)) {  //===== Check the Student ID is valid or not
+		$tid = $_SESSION['tid'];
+
+        if(checkFourtyStud($cid)) {  //===== Check if the no. of students is fourty or not =====
+            if (checkUniqueId($tid, $stuId, $cid)) {  //===== Check the student has already added or not =====
+                /**
+                 * Adding Student in the Course
+                 */
+				addStudent($tid, $stuId, $cid);
+				attendence($stuId, $cid);
+				addMarks($stuId, $cid);
+				
 				echo '<script language="javascript">
-							alert("ID has already inserted !!");
-							window.location="'.SERVER.'/addStudent?id='.$cid.'";
-						  </script>';
+					      alert("Successfully Added");
+						  window.location="'.SERVER.'/teacherCourse?id='.$cid.'";
+					  </script>';
+			} else {
+                echo '<script language="javascript">
+                          alert("ID has already inserted !!");
+						  window.location="'.SERVER.'/addStudent?id='.$cid.'";
+					  </script>';
 			}
+		} else {
+            echo '<script language="javascript">
+				      alert("You cannot enter more than 40 students !!");
+					  window.location="'.SERVER.'/teacherCourse?id='.$cid.'";
+				  </script>';
 		}
-		else{
-			echo '<script language="javascript">
-							alert("Student ID is Invalid !!");
-							window.location="'.SERVER.'/addStudent?id='.$cid.'";
-						  </script>';
-		}
+	} else {
+		echo '<script language="javascript">
+			      alert("Student ID is Invalid !!");
+				  window.location="'.SERVER.'/addStudent?id='.$cid.'";
+			  </script>';
 	}
-	else{
-		header('Location: '.SERVER.'');
-	}
-?>
+} else {
+	header('Location: '.SERVER.'');
+}
