e553b7f5caabd0e839e59be88635a7aca8eb9a91
Saleh Ahmad <salehoyon@hotmail.com>
2016-05-16 01:01:14 +0600

    * PSR Update

--- controller/removeStudentSuccess.php
+++ controller/removeStudentSuccess.php
@@ -1,21 +1,25 @@
 <?php
-	require '../model/db.php';
-	require 'define.php';
 
-	if(isset($_POST['dltBtn'])){
-		$cid = $_GET['id1'];
-		$sid = $_GET['id2'];
-		deleteStudent($cid, $sid);
-		deleteStudentAttendence($cid, $sid);
-		removeStudentExam($cid, $sid);
-		removeStudentQuizTerm($cid, $sid);
-		
-		echo '<script language="javascript">
-				alert("Successfully Removed !!");
-				window.location="'.SERVER.'/studentlist?id='.$cid.'";
-			  </script>';
-	}
-	else{
-		header('Location: '.SERVER.'');
-	}
-?>
+/*===== Required Files =====*/
+require '../model/db.php';
+require 'define.php';
+
+if (isset($_POST['dltBtn'])) {
+	$cid = $_GET['id1'];
+	$sid = $_GET['id2'];
+
+    /**
+     * Removing Students from all the Records
+     */
+	deleteStudent($cid, $sid);
+	deleteStudentAttendence($cid, $sid);
+	removeStudentExam($cid, $sid);
+	removeStudentQuizTerm($cid, $sid);
+
+	echo '<script language="javascript">
+		      alert("Successfully Removed !!");
+			  window.location="'.SERVER.'/studentlist?id='.$cid.'";
+		  </script>';
+} else {
+	header('Location: '.SERVER.'');
+}
