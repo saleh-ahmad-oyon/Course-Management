a5901c253ef83be522aa4a740649528323e59b92
Saleh Ahmad <salehoyon@hotmail.com>
2016-05-16 01:21:49 +0600

    * PSR Update

--- controller/stuMarksDeleteSubmit.php
+++ controller/stuMarksDeleteSubmit.php
@@ -1,21 +1,23 @@
 <?php
-	require '../model/db.php';
-	require 'define.php';
 
-	if(isset($_POST['dltMarks'])){
-		$cid = $_GET['id1'];
-		$studid = $_GET['id2'];
-		if(!empty($_POST['check_dlt'])){
-			foreach($_POST['check_dlt'] as $eid){
-				deleteExamMarks($eid);
-			}
-		}
-		echo '<script language="javascript">
-				alert("Successfully Deleted !!");
-				window.location="'.SERVER.'/detailsMark?id1='.$cid.'&id2='.$studid.'";
-			  </script>';
-	}
-	else{
-		header('Location: '.SERVER.'');
-	}
-?>
+/*===== Required Files =====*/
+require '../model/db.php';
+require 'define.php';
+
+if (isset($_POST['dltMarks'])) {
+    $cid    = $_GET['id1'];
+    $studid = $_GET['id2'];
+
+    if (!empty($_POST['check_dlt'])) {
+        foreach ($_POST['check_dlt'] as $eid) {
+            deleteExamMarks($eid);
+        }
+    }
+
+    echo '<script language="javascript">
+              alert("Successfully Deleted !!");
+              window.location="'.SERVER.'/detailsMark?id1='.$cid.'&id2='.$studid.'";
+          </script>';
+} else {
+    header('Location: '.SERVER.'');
+}
