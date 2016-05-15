5ef5696815eb1bf5eb6c984bde223429beda378d
Saleh Ahmad <salehoyon@hotmail.com>
2016-05-15 23:25:41 +0600

    * Remove show button panel from jquery UI

--- addTeacherStudent.php
+++ addTeacherStudent.php
@@ -20,7 +20,6 @@
                     showOtherMonths: true,
                     changeMonth: true,
                     changeYear: true,
-                    showButtonPanel: true,
                     dateFormat: "dd/mm/yy",
                     minDate: '01/01/1900',
                     maxDate: "+0D"
