3db5a8d5452fec82c923e90e334fbdb94f0114ad
Saleh Ahmad <salehoyon@hotmail.com>
2016-05-16 00:07:15 +0600

    * PSR Update

--- controller/addMarksByTeacher.php
+++ controller/addMarksByTeacher.php
@@ -1,48 +1,44 @@
 <?php
-	require '../model/db.php';
-	require 'define.php';
-	
-	if(isset($_POST['addMidBestTwo'])){
-		$cid = $_POST['cid'];
-		$sid = $_POST['sid'];
-		$mark = $_POST['mark'];
 
-		addMidBestTwo($cid, $sid, $mark);
-		header('Location: '.SERVER.'/detailsMark?id1='.$cid.'&id2='.$sid.'');
-	}
-	elseif(isset($_POST['addFinalBestTwo'])){
-		$cid = $_POST['cid'];
-		$sid = $_POST['sid'];
-		$mark = $_POST['mark'];
+/*===== Required Files =====*/
+require '../model/db.php';
+require 'define.php';
 
-		addFinalBestTwo($cid, $sid, $mark);
-		header('Location: '.SERVER.'/detailsMark?id1='.$cid.'&id2='.$sid.'');
-	}
-	elseif(isset($_POST['addMidTotal'])){
-		$cid = $_POST['cid'];
-		$sid = $_POST['sid'];
-		$mark = $_POST['mark'];
+if (isset($_POST['addMidBestTwo'])) {  //===== Add Best Two Quiz Marks in Mid =====
+	$cid  = $_POST['cid'];
+	$sid  = $_POST['sid'];
+	$mark = $_POST['mark'];
 
-		addMidTotal($cid, $sid, $mark);
-		header('Location: '.SERVER.'/detailsMark?id1='.$cid.'&id2='.$sid.'');
-	}
-	elseif(isset($_POST['addFinalTotal'])){
-		$cid = $_POST['cid'];
-		$sid = $_POST['sid'];
-		$mark = $_POST['mark'];
+	addMidBestTwo($cid, $sid, $mark);
+	header('Location: '.SERVER.'/detailsMark?id1='.$cid.'&id2='.$sid.'');
+} elseif (isset($_POST['addFinalBestTwo'])) {  //===== Add Best Two Quiz Marks in Final =====
+	$cid  = $_POST['cid'];
+	$sid  = $_POST['sid'];
+	$mark = $_POST['mark'];
 
-		addFinalTotal($cid, $sid, $mark);
-		header('Location: '.SERVER.'/detailsMark?id1='.$cid.'&id2='.$sid.'');
-	}
-	elseif(isset($_POST['addGrandTotal'])){
-		$cid = $_POST['cid'];
-		$sid = $_POST['sid'];
-		$mark = $_POST['mark'];
-		
-		addGrandTotal($cid, $sid, $mark);
-		header('Location: '.SERVER.'/detailsMark?id1='.$cid.'&id2='.$sid.'');
-	}
-	else{
-		header('Location: '.SERVER.'');
-	}
-?>
+	addFinalBestTwo($cid, $sid, $mark);
+	header('Location: '.SERVER.'/detailsMark?id1='.$cid.'&id2='.$sid.'');
+} elseif (isset($_POST['addMidTotal'])) {  //===== Add Total Marks in Mid =====
+	$cid  = $_POST['cid'];
+	$sid  = $_POST['sid'];
+	$mark = $_POST['mark'];
+
+	addMidTotal($cid, $sid, $mark);
+	header('Location: '.SERVER.'/detailsMark?id1='.$cid.'&id2='.$sid.'');
+} elseif (isset($_POST['addFinalTotal'])) {  //===== Add Total Marks in Final =====
+	$cid  = $_POST['cid'];
+	$sid  = $_POST['sid'];
+	$mark = $_POST['mark'];
+
+	addFinalTotal($cid, $sid, $mark);
+	header('Location: '.SERVER.'/detailsMark?id1='.$cid.'&id2='.$sid.'');
+} elseif (isset($_POST['addGrandTotal'])) {  //===== Add Grand Total Marks =====
+	$cid  = $_POST['cid'];
+	$sid  = $_POST['sid'];
+	$mark = $_POST['mark'];
+
+	addGrandTotal($cid, $sid, $mark);
+	header('Location: '.SERVER.'/detailsMark?id1='.$cid.'&id2='.$sid.'');
+} else {
+	header('Location: '.SERVER.'');
+}
