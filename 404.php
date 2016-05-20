85987c63a805b70877081d7eaa83f177eddccaa2
Saleh Ahmad <salehoyon@hotmail.com>
2016-05-20 22:11:03 +0600

    * Add 404 page

---
+++ 404.php
@@ -0,0 +1,22 @@
+<?php require_once 'controller/define.php';?>
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<title>Page Not Found</title>
+		<link href="<?php echo SERVER; ?>/assets/img/aiub.png" rel="shortcut icon" />
+		<link href="<?php echo SERVER; ?>/assets/css/404.css" rel="stylesheet" type="text/css"  media="all" />
+	</head>
+	<body>
+		<!--start-wrap--->
+		<div class="wrap">
+			<!--start-content------>
+			<div class="content">
+				<img src="<?php echo SERVER; ?>/assets/img/error/error-img.png" title="error" />
+				<p><span><label>O</label>hh.....</span>You Requested the page that is no longer There.</p>
+				<a href="<?php echo SERVER; ?>">Back To Home</a>
+   			</div>
+			<!--End-Cotent------>
+		</div>
+		<!--End-wrap--->
+	</body>
+</html>
