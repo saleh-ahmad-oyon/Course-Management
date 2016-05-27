f7dfa26cbbb062efb71a515d977e12f3bb076537
Saleh Ahmad <salehoyon@hotmail.com>
2016-05-27 22:51:06 +0600

    * Updating...

--- controller/addCourseTeacher.php
+++ controller/addCourseTeacher.php
@@ -9,50 +9,51 @@
 require '../model/db.php';
 require 'define.php';
 
-/**
- * Authority Assigns Course to the Teacher
- */
-if (isset($_POST['addCot'])) {
-    $teacherInfo = explode(" ", $_POST['teacher']);
-    $tAiubId     = $teacherInfo[0];
-
-    if (isset($_POST['teacher'])) {
-        $tid = getTId($tAiubId);
-    }
-
-    $course = $_POST['subject'];
-
-    if ($_POST['teacher'] != '' && $_POST['subject'] != '') {
-
-        /** Checking if the Course is Assigned before to this Teacher */
-        if (!checkDuplicateCourse($tid, $course)) {
-
-            /** Checking if the Course is assigned before */
-            if (!checkCourse($course)) {
-
-                /** Assigning Course to the Teacher */
-                insertCourseTeacher($course, $tid);
-                
-                echo '<script language="javascript">
-                          alert("Successfully Assigned !!");
-                          window.location="'.SERVER.'/teacher/subject";
-                      </script>';
-            } else {
-                echo '<script language="javascript">
-                          alert("A teacher already assigned in this course !!");
-                          window.location="'.SERVER.'/teacher/subject";
-                      </script>';
-            }
-        } else {
-            echo '<script language="javascript">
-                      alert("Duplicate Data !! Can\'t insert !!");
-                      window.location="'.SERVER.'/teacher/subject";
-                  </script>';
-        }
-    } else {
-        echo '<script language="javascript">
-                  alert("You\'ve to fill both teacher and course");
-                  window.location="'.SERVER.'/teacher/subject";
-              </script>';
-    }
+if (!isset($_POST['addCot'])) {
+    /** @Link 404 Page */
+    header('Location: '.SERVER.'/404');
+    return;
 }
+
+$teacherInfo = explode(" ", $_POST['teacher']);
+$tAiubId     = $teacherInfo[0];
+
+if (isset($_POST['teacher'])) {
+    $tid = getTId($tAiubId);
+}
+
+$course = $_POST['subject'];
+
+if ($_POST['teacher'] == '' && $_POST['subject'] == '') {
+    echo '<script language="javascript">
+              alert("You\'ve to fill both teacher and course");
+              window.location="'.SERVER.'/teacher/subject";
+          </script>';
+    return;
+}
+
+/** Checking if the Course is Assigned before to this Teacher */
+if (!checkDuplicateCourse($tid, $course)) {
+    echo '<script language="javascript">
+              alert("The faculty member is already assigned in the course !!");
+              window.location="'.SERVER.'/teacher/subject";
+          </script>';
+    return;
+}
+
+/** Checking if the Course is assigned before */
+if (!checkCourse($course)) {
+    echo '<script language="javascript">
+              alert("Another Faculty member is already assigned in the course !!");
+              window.location="'.SERVER.'/teacher/subject";
+          </script>';
+    return;
+}
+
+/** Assigning Course to the Teacher */
+insertCourseTeacher($course, $tid);
+
+echo '<script language="javascript">
+          alert("Successfully Assigned !!");
+          window.location="'.SERVER.'/teacher/subject";
+      </script>';
