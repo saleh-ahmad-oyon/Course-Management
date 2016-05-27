624365516d3e64f35a685a91e07361ab4ac59b88
Saleh Ahmad <salehoyon@hotmail.com>
2016-05-27 21:56:03 +0600

    * Updating...

--- controller/removeStudent.php
+++ controller/removeStudent.php
@@ -9,22 +9,28 @@
 require '../model/db.php';
 require 'define.php';
 
-if (isset($_POST['dltBtn'])) {
-	$cid = $_GET['id1'];
-	$sid = $_GET['id2'];
-
-    /**
-     * Removing Students from all the Records
-     */
-	deleteStudent($cid, $sid);
-	deleteStudentAttendence($cid, $sid);
-	removeStudentExam($cid, $sid);
-	removeStudentQuizTerm($cid, $sid);
-
-	echo '<script language="javascript">
-		      alert("Successfully Removed !!");
-			  window.location="'.SERVER.'/course/'.$cid.'/students";
-		  </script>';
-} else {
-	header('Location: '.SERVER.'');
+if (!isset($_POST['dltBtn'])) {
+    /** @Link 404 Page */
+    header('Location: '.SERVER.'/404');
+    return;
 }
+
+/**
+ * @var int $cid     Course ID
+ * @var int $sid     Student ID
+ */
+$cid = $_GET['id1'];
+$sid = $_GET['id2'];
+
+/**
+ * Removing Students from all the Records
+ */
+deleteStudent($cid, $sid);
+deleteStudentAttendence($cid, $sid);
+removeStudentExam($cid, $sid);
+removeStudentQuizTerm($cid, $sid);
+
+echo '<script language="javascript">
+          alert("Successfully Removed !!");
+          window.location="'.SERVER.'/course/'.$cid.'/students";
+      </script>';
