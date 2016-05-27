0062ba086d692133dd3c35ee9f361d4b7fae04cb
Saleh Ahmad <salehoyon@hotmail.com>
2016-05-27 19:29:57 +0600

    * Updating...

--- session.php
+++ session.php
@@ -12,70 +12,69 @@ session_start();
 require 'model/db.php';
 require 'controller/define.php';
 
-if (isset($_POST['loginbtn'])) {
+if (!isset($_POST['loginbtn'])) {
+    header('Location: '.SERVER.'');
+}
+
+/** @var string $user     AIUB ID*/
+$user = $_POST['user'];
+
+/**
+ * Check if the AIUB ID pattern is matching with XX-XXXXX-X or XXXX-XXXX-X or XXXX-XXX-X
+ */
+if (!preg_match('/^\d{2}\-\d{5}\-\d{1}$|^\d{4}\-\d{3,4}\-\d{1}$/',$user)) {
+    header('Location: '.SERVER.'');
+}
+
+/** @var string     $pass    Password */
+$pass = $_POST['pass'];
+
+/** Check if the AIUB ID pattern is matching with XXXX-XXXX-1 for Authority */
+if (preg_match('/^\d{4}\-\d{4}\-1$/',$user)) {
+    $authority = check_authority_login($user, $pass);
+
+    if (!$authority) {
+        header('Location: '.SERVER.'?err=1');
+    }
 
     /**
-     * @var string     $user    AIUB ID
-     * @var string     $pass    Password
+     * @var string     $_SESSION['authority']    Saving Authority's AIUB ID
+     * @var int        $_SESSION['aid']          Saving Authority's ID
      */
-	$user = $_POST['user'];
-	$pass = $_POST['pass'];
+    $_SESSION['authority'] = $user;
+    $_SESSION['aid']       = authority_id($user);
+
+    header('Location: '.SERVER.'');
+} elseif (preg_match('/^\d{4}\-\d{3,4}\-[23]$/',$user)) { /** Check if the AIUB ID pattern is matching with XXXX-XXXX-2 or XXXX-XXX-2 for Teacher */
+    $teacher = check_teacher_login($user, $pass);
+
+    if (!$teacher) {
+        header('Location: '.SERVER.'?err=1');
+    }
 
     /**
-     * Check if the AIUB ID pattern is matching with XX-XXXXX-X or XXXX-XXXX-X
+     * @var string     $_SESSION['teacher']    Saving Teacher's AIUB ID
+     * @var int        $_SESSION['tid']        Saving Teacher's ID
      */
-	if (preg_match('/^\d{2}\-\d{5}\-\d{1}$|^\d{4}\-\d{3,4}\-\d{1}$/',$user)) {
+    $_SESSION['teacher'] = $user;
+    $_SESSION['tid']     = teacher_id($user);
 
-        /** Check if the AIUB ID pattern is matching with XXXX-XXXX-1 for Authority */
-		if (preg_match('/^\d{4}\-\d{4}\-1$/',$user)) {
-			$authority = check_authority_login($user, $pass);
-			if ($authority) {
+    header('Location: '.SERVER.'');
+} elseif (preg_match('/^\d{2}\-\d{5}\-\d{1}$/',$user)) {  /** Check if the AIUB ID pattern is matching with XX-XXXXX-1 for Student */
+    $student = check_stud_login($user, $pass);
 
-                /**
-                 * @var string     $_SESSION['authority']    Saving Authority's AIUB ID
-                 * @var int        $_SESSION['aid']          Saving Authority's ID
-                 */
-				$_SESSION['authority'] = $user;
-				$_SESSION['aid']       = authority_id($user);
-                
-				header('Location: '.SERVER.'');
-			} else {
-				header('Location: '.SERVER.'?err=1');
-			}		
-		} elseif (preg_match('/^\d{4}\-\d{3,4}\-[23]$/',$user)) { /** Check if the AIUB ID pattern is matching with XXXX-XXXX-2 or XXXX-XXX-2 for Teacher */
-			$teacher = check_teacher_login($user, $pass);
-			if ($teacher) {
+    if (!$student) {
+        header('Location: '.SERVER.'?err=1');
+    }
 
-                /**
-                 * @var string     $_SESSION['teacher']    Saving Teacher's AIUB ID
-                 * @var int        $_SESSION['tid']        Saving Teacher's ID
-                 */
-                $_SESSION['teacher'] = $user;
-				$_SESSION['tid']     = teacher_id($user);
-				
-				header('Location: '.SERVER.'');
-			} else {
-				header('Location: '.SERVER.'?err=1');
-			}		
-		} elseif (preg_match('/^\d{2}\-\d{5}\-\d{1}$/',$user)) {  /** Check if the AIUB ID pattern is matching with XX-XXXXX-1 for Student */
-			$student = check_stud_login($user, $pass);
-			if ($student) {
+    /**
+     * @var string     $_SESSION['stud']    Saving Student's AIUB ID
+     * @var int        $_SESSION['sid']     Saving Student's ID
+     */
+    $_SESSION['stud'] = $user;
+    $_SESSION['sid']  = student_id($user);
 
-                /**
-                 * @var string     $_SESSION['stud']    Saving Student's AIUB ID
-                 * @var int        $_SESSION['sid']     Saving Student's ID
-                 */
-                $_SESSION['stud'] = $user;
-				$_SESSION['sid']  = student_id($user);
-                
-				header('Location: '.SERVER.'');
-			} else {
-				header('Location: '.SERVER.'?err=1');
-			}
-		} else {
-			header('Location: '.SERVER.'?err=2');
-		}
-	} else {
-		header('Location: '.SERVER.'');
-	}
+    header('Location: '.SERVER.'');
+} else {
+    header('Location: '.SERVER.'?err=2');
 }
