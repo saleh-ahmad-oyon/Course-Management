5a3655160879fff510c8874ed7a2a17e1e2e0ed7
Saleh Ahmad <salehoyon@hotmail.com>
2016-05-28 14:27:00 +0600

    * Updating...

--- controller/define.php
+++ controller/define.php
@@ -7,5 +7,5 @@
 
 /** Define Constants */
 define('SERVER', 'http://localhost/course');
-define('STUDENTPP', ''.SERVER.'/assets/img/student');
-define('TEACHERPP', ''.SERVER.'/assets/img/teacher');
+define('STUDENTPP', SERVER.'/assets/img/student');
+define('TEACHERPP', SERVER.'/assets/img/teacher');
--- controller/dltTeacherCourse.php
+++ controller/dltTeacherCourse.php
@@ -9,20 +9,25 @@
 require '../model/db.php';
 require 'define.php';
 
-if (isset($_POST['dltTeacher'])) {
-    $cid = $_POST['id'];
-
-    /**
-     * Removing Teacher from a Course and all related records
-     */
-    deleteTeacherCourse($cid);
-    deleteStudentCourse($cid);
-    deleteCourseStudentAttendence($cid);
-    removeCourseStudentExam($cid);
-    removeCourseStudentQuizTerm($cid);
-    
-    echo '<script language="javascript">
-              alert("Successfully Removed !!");
-              window.location="'.SERVER.'/teacher/subject";
-          </script>';
+if (!isset($_POST['dltTeacher'])) {
+    /** @Link 404 Page */
+    header('Location: '.SERVER.'/404');
+    return;
 }
+
+/** @var int $cid     Course ID */
+$cid = $_POST['id'];
+
+/**
+ * Removing Teacher from a Course and all related records
+ */
+deleteTeacherCourse($cid);
+deleteStudentCourse($cid);
+deleteCourseStudentAttendence($cid);
+removeCourseStudentExam($cid);
+removeCourseStudentQuizTerm($cid);
+
+echo '<script language="javascript">
+          alert("Successfully Removed !!");
+          window.location="'.SERVER.'/teacher/subject";
+      </script>';
