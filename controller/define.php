fca5295ca8fcd6c509f048558bfedc07a88746ec
Saleh Ahmad <salehoyon@hotmail.com>
2016-06-01 00:01:22 +0600

    v 1.0

--- controller/define.php
+++ controller/define.php
@@ -6,6 +6,12 @@
  */
 
 /** Define Constants */
-define('SERVER', 'http://'.$_SERVER['SERVER_NAME'].'/course');
+
+/** Define Protocol */
+define('PROTOCOL', ((!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] != 'off') ||
+    $_SERVER['SERVER_PORT'] == 443) ? "https://" : "http://");
+
+/** Define Server Name */
+define('SERVER', PROTOCOL.$_SERVER['SERVER_NAME'].'/course');
 define('STUDENTPP', SERVER.'/assets/img/student');
 define('TEACHERPP', SERVER.'/assets/img/teacher');
