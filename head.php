8fcc40944f2974c7b1f930e0987a1a262fda3944
Saleh Ahmad <salehoyon@hotmail.com>
2016-06-01 22:29:55 +0600

    v 1.0

--- head.php
+++ head.php
@@ -1,17 +1,20 @@
-<meta  charset="UTF-8"/>
+<meta charset="UTF-8"/>
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<meta name="description" content="It is an application that can be used to manage a course according to AIUB(American International University-Bangladesh) regulation." />
-<meta name="keywords" content="Lonely,L0onely,aiub,AIUB,AIUB Course,courses,course management" />
+<meta name="description" content="It is a demo php project that can be used to manage a course according to AIUB(American International University-Bangladesh) regulation. It was done by a student of AIUB" />
+<meta name="keywords" content="Lonely,L0onely,noob lonely,aiub,AIUB,AIUB Course,courses,course management,noob,nooblonely,teacher,student,authority,American International University Bangladesh(AIUB),American International University-Bangladesh,Mid term Project,PHP,PHP Project,Bootstrap,Bootstrap Used Project,Project in AIUB,project done by aiubian" />
 <meta name="language" content="English" />
 <meta name="country" content="Bangladesh" />
 <meta name="Title" content="Course Management" />
-<meta name="Owner" content="L0onely" />
+<meta name="Owner" content="Noob L0onely" />
 <meta name="Copyright" content="<?php echo date("Y") ?> Course Management all rights reserved" />
 <meta name="Author" content="Saleh Ahmad" />
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta http-equiv="refresh" content="30">
+<meta name="robots" content="index, nofollow">
 
 <title>Course Management</title>
-<link href="<?= SERVER; ?>/assets/img/aiub.png" rel="shortcut icon" />
+<link href="<?= SERVER; ?>/assets/img/aiub.png" rel="icon" />
+<link href="https://www.linkedin.com/in/saleh-ahmad-917b0532" rel="publisher"/>
 <link href="<?= SERVER; ?>/assets/css/bootstrap-3.3.6-dist/css/bootstrap.min.css" rel="stylesheet"/>
 <link href="<?= SERVER; ?>/assets/css/font-awesome-4.5.0/css/font-awesome.min.css" rel="stylesheet"/>
 <link href="<?= SERVER; ?>/assets/css/custom.php" rel="stylesheet"/>
