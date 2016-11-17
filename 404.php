5f933ee6077b63584b302149fbb4630d7f23f1b1
Saleh Ahmad <salehoyon@hotmail.com>
2016-11-18 04:28:58 +0600

    Update coding convention

--- 404.php
+++ 404.php
@@ -1,19 +1,18 @@
 <?php require_once 'controller/define.php';?>
 <!DOCTYPE HTML>
-<html lang="en-US">
+<html>
 <head>
   <title>Page Not Found</title>
   <link href="<?= SERVER; ?>/assets/img/aiub.png" rel="icon" />
   <link href="<?= SERVER; ?>/assets/css/404.css" rel="stylesheet" type="text/css"  media="all" />
 </head>
 <body>
-  <!--start-wrap--->
   <div class="wrap">
 	<div class="content">
 	  <img src="<?= SERVER; ?>/assets/img/error/error-img.png" title="error" />
 	  <p><span><label>O</label>hh.....</span>You Requested the page that is no longer There.</p>
 	  <a href="<?= SERVER; ?>">Back To Home</a>
 	</div><!-- /.content -->
-  </div><!--/#wrap--->
+  </div><!-- /#wrap -->
 </body>
 </html>
