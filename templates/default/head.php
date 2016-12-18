cb1a7d2a1410c52d78da305bf596499443294e8d
Saleh Ahmad <salehoyon@hotmail.com>
2016-12-18 22:26:55 +0600

    Update meta

--- templates/default/head.php
+++ templates/default/head.php
@@ -15,6 +15,11 @@ aiub mini portal,project done by aiubian,php with bootstrap,varsity portal proje
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <!--<meta http-equiv="refresh"         content="30">-->
 <meta name="robots"                content="index, nofollow">
+
+<!-- Canonical SEO -->
+<link rel="canonical" href="http://www.nooblonely.com">
+
+<!-- Open Graph data -->
 <meta property="og:locale"         content="en_US" />
 <meta property="og:url"            content="<?= SERVER; ?>" />
 <meta property="og:type"           content="website" />
@@ -22,14 +27,37 @@ aiub mini portal,project done by aiubian,php with bootstrap,varsity portal proje
 <meta property="og:site_name"      content="Course Management"
 <meta property="og:description"    content="It is a demo php project that can be used to manage a course according to AIUB(American International University-Bangladesh) regulation. It was done by a student of AIUB" />
 <meta property="og:image"          content="<?= SERVER; ?>/assets/img/aiub.png" />
+
+<!-- Twitter Card data -->
 <meta name="twitter:card"          content="Course_Management" />
 <meta name="twitter:description"   content="It is a demo php project that can be used to manage a course according to AIUB(American International University-Bangladesh) regulation. It was done by a student of AIUB." />
 <meta name="twitter:title"         content="Course Management for AIUB" />
-<meta name="twitter:site"          content="@l0onely" />
+<meta name="twitter:site"          content="@Nissongo_10" />
 <meta name="twitter:image"         content="<?= SERVER; ?>/assets/img/aiub.png" />
-<meta name="twitter:creator"       content="@l0onely" />
+<meta name="twitter:creator"       content="@Nissongo_10" />
 
-<link rel="icon"       href="<?= SERVER; ?>/assets/img/aiub.png" />
+<!-- favicon -->
+<link rel="apple-touch-icon-precomposed" sizes="57x57" href="favicons/apple-touch-icon-57x57.png" />
+<link rel="apple-touch-icon-precomposed" sizes="114x114" href="favicons/apple-touch-icon-114x114.png" />
+<link rel="apple-touch-icon-precomposed" sizes="72x72" href="favicons/apple-touch-icon-72x72.png" />
+<link rel="apple-touch-icon-precomposed" sizes="144x144" href="favicons/apple-touch-icon-144x144.png" />
+<link rel="apple-touch-icon-precomposed" sizes="60x60" href="favicons/apple-touch-icon-60x60.png" />
+<link rel="apple-touch-icon-precomposed" sizes="120x120" href="favicons/apple-touch-icon-120x120.png" />
+<link rel="apple-touch-icon-precomposed" sizes="76x76" href="favicons/apple-touch-icon-76x76.png" />
+<link rel="apple-touch-icon-precomposed" sizes="152x152" href="favicons/apple-touch-icon-152x152.png" />
+<link rel="icon" type="image/png" href="favicons/favicon-196x196.png" sizes="196x196" />
+<link rel="icon" type="image/png" href="favicons/favicon-96x96.png" sizes="96x96" />
+<link rel="icon" type="image/png" href="favicons/favicon-32x32.png" sizes="32x32" />
+<link rel="icon" type="image/png" href="favicons/favicon-16x16.png" sizes="16x16" />
+<link rel="icon" type="image/png" href="favicons/favicon-128.png" sizes="128x128" />
+<meta name="application-name" content="&nbsp;"/>
+<meta name="msapplication-TileColor" content="#FFFFFF" />
+<meta name="msapplication-TileImage" content="favicons/mstile-144x144.png" />
+<meta name="msapplication-square70x70logo" content="favicons/mstile-70x70.png" />
+<meta name="msapplication-square150x150logo" content="favicons/mstile-150x150.png" />
+<meta name="msapplication-wide310x150logo" content="favicons/mstile-310x150.png" />
+<meta name="msapplication-square310x310logo" content="favicons/mstile-310x310.png" />
+
 <link rel="publisher"  href="https://www.linkedin.com/in/saleh-ahmad-917b0532" />
 
 <link rel="stylesheet" media='all' href="<?= SERVER; ?>/assets/css/bootstrap-3.3.6-dist/css/bootstrap.min.css" />
